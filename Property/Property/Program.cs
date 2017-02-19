82f2e80bd9216016943ab7f9273aa4a4aff4eb3a
sandro sviben <sandro.sviben@hotmail.com>
2017-02-19 15:52:57 +0100

    Primjer property

---
+++ Property/Property/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Property example
+
+
+namespace Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
