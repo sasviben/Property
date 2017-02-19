13abf68667dcf23aefb7c149f27adfc82388c371
sandro sviben <sandro.sviben@hotmail.com>
2017-02-19 16:18:00 +0100

    example of my property methods

--- Property/Property/Program.cs
+++ Property/Property/Program.cs
@@ -9,10 +9,61 @@ using System.Threading.Tasks;
 
 namespace Property
 {
+    public class Student
+    {
+        private int _id;
+        private string _name;
+        private int _passMark = 35;
+
+        // deklaracija metode za dohvaćanje _passMark
+        public int GetPassMark()
+        {
+            return this._passMark;
+        }
+        // deklaracija metode za inicijalizaciju _name
+        public void SetName(string name)
+        {
+            if(string.IsNullOrEmpty(name))
+            {
+                throw new Exception("Name cannot be null or empty");
+            }
+            this._name = name;
+        }
+        // deklaracija metode za dohvaćanje _name
+        public string GetName()
+        {
+            return string.IsNullOrEmpty(this._name) ? "No name" : this._name;
+        }
+        // deklaracija metode za inicijalizaciju _id
+        public void SetId(int id)
+        {
+            if(id <= 0)
+            {
+                throw new Exception("Student id cannot be negativ or 0");
+            }
+            this._id = id;
+        }
+        // deklaracija metode za dohvaćanje _id
+        public int GetId()
+        {
+            return this._id;
+        }
+    }
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            Student S1 = new Student();
+            S1.SetId(101);
+            S1.SetName("sandro");
+
+            Console.WriteLine("Student Id = {0}", S1.GetId());
+            Console.WriteLine("Student Name = {0}", S1.GetName());
+            Console.WriteLine("Student PassMark = {0}", S1.GetPassMark());
+
         }
     }
 }
